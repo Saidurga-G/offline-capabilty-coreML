25d22777c438280db52059748aeaa59230526da9
Sai Durga, Gudivada <gudivada.saidurga@aa.com>
2026-02-02 12:39:33 +0530

    adding all the files to repo

---
+++ offline-capabilty-coreML/offline_capabilty_coreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  offline_capabilty_coreMLApp.swift
+//  offline-capabilty-coreML
+//
+//  Created by Sai Durga, Gudivada on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct offline_capabilty_coreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
